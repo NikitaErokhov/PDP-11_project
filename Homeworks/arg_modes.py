f958f42b1c85f61002b7143c4e1b80df47020a86
erokhov.nm <erokhov.nm@phystech.edu>
2025-09-21 19:26:27 +0300

    fix: fixing get_mode_reg_from_args function. add: new tests for homework 2

--- Homeworks/arg_modes.py
+++ Homeworks/arg_modes.py
@@ -1,25 +1,49 @@
 import pyparsing as pp
+from re import VERBOSE
 
-args = ['(R3)+', '(R2)', '(R1)']
 
+def get_mode_reg_from_args(args: list[str], test: bool = False):
 
-def get_mode_reg_from_args(args: list[str]):
-    binary_result = []
-    argument_name = pp.Word(pp.alphanums)
-    modes_list = [("(" + argument_name + ")+").setParseAction(pp.replaceWith(2)),
-                  ("(" + argument_name + ")").setParseAction(pp.replaceWith(1)),
-                  argument_name.setParseAction(pp.replaceWith(0)),
-                  ]
+    result = dict()
+    argument_name = pp.Word(pp.alphanums+'#')
+
+    modes_list = [
+        ("@2(" + argument_name + ")").setParseAction(pp.replaceWith(7)),
+        ("2(" + argument_name + ")").setParseAction(pp.replaceWith(6)),
+        ("#" + pp.Word(pp.nums)).setParseAction(pp.replaceWith("2 const")),
+        ("@#" + pp.Word(pp.nums)
+         ).setParseAction(pp.replaceWith("3 mem[mem[v]]")),
+        ("@" + pp.Word(pp.nums)).setParseAction(pp.replaceWith("7 mem[v]")),
+        pp.Word(pp.nums).setParseAction(pp.replaceWith("6 mem[v]")),
+        ("@-(" + argument_name + ")").setParseAction(pp.replaceWith(5)),
+        ("-(" + argument_name + ")").setParseAction(pp.replaceWith(4)),
+        ("@(" + argument_name + ")+").setParseAction(pp.replaceWith(3)),
+        ("(" + argument_name + ")+").setParseAction(pp.replaceWith(2)),
+        ("(" + argument_name + ")").setParseAction(pp.replaceWith(1)),
+        argument_name.setParseAction(pp.replaceWith(0)),
+    ]
 
     modes_to_search = pp.MatchFirst(modes_list)
-    register_name = pp.Optional(pp.Suppress('('))+pp.Word("R01234567")
+
+    register_name = pp.Optional(pp.Suppress(
+        pp.Word('@2-+('))) + pp.Regex(r"""
+                (([rR]+[0-7]))
+                |
+                (pc) | (PC)
+                |
+                (sp) | (SP)""", flags=VERBOSE) +\
+        pp.Optional(pp.Suppress(pp.Word('+)')))
+
+    const_name = pp.Optional(pp.Suppress(pp.Word('#@'))) + \
+        pp.Word(pp.nums)
+
+    value_name = register_name | const_name
 
     for arg in args:
-        reg_name = register_name.parseString(arg)[0]
-        reg_mode = modes_to_search.parseString(arg)[0]
-        binary_result.append(f"{reg_mode:03b}{int(reg_name[1]):03b}")
+        val_mode = modes_to_search.parseString(arg)[0]
+        val_name = value_name.parseString(arg)[0]
+        result[val_name.upper()] = val_mode
+        if (test):
+            print(f"from {arg} get mode {val_mode} for {val_name.upper()}")
 
-    print(binary_result)
-
-
-get_mode_reg_from_args(args)
+    return result
